0c398b8898f7406f04c5d345ad2b57dfd3d63f4c
wyeung30 <301448925@cencol.ca>
2025-04-07 15:59:01 -0400

    q1

--- examPractice/Program.cs
+++ examPractice/Program.cs
@@ -10,8 +10,9 @@ namespace examPractice
     {
         static void Main(string[] args)
         {
-         //   hashSet();
-            dictionary();
+            //   hashSet();
+          //  dictionary();
+            orinigalCountry();
         }
 
 
@@ -44,7 +45,8 @@ namespace examPractice
         // 24: Lemon
         // 25: Tomato
 
-        static void hashSet() {
+        static void hashSet()
+        {
             int counter = 0;
             HashSet<String> fruit = new HashSet<String>();
             fruit.Add("Banana");
@@ -78,48 +80,115 @@ namespace examPractice
                 counter++;
                 Console.WriteLine($"{counter} : {item}");
             }
-          
+
         }
 
         //Question 1b: Create a Dictionary of Countries with the fruits exports (as a list)
- //       Type: System.Collections.Generic.Dictionary`2[System.String, System.Collections.Generic.List`1[System.String]]
- // 1: Guyana: Banana, Plaintain, Mango, Cashew
- // 2: New Zeeland: Kiwi, Apple, Avocado, Cherry
- // 3: Ecuador: Banana
- // 4: Spain: Persimmon, Orange, Mandarin
- // 5: Canada: Blueberry
- // 6: Russia: Strawberry
- // 7: Israel: Persimmon, Dates, Pomegranates
- // 8: China: Dragon Fruit, Apple, Pear, Pomelo, Cherry, Mandarin
- // 9: Chile: Figs, Avocado, Blueberry, Cherry, Grape, Plum
- //10: Guatemala: Pineapple, Banana, Papaya
- //11: Mexico: Lemon, Tomato, Blueberry, Avocado
+        //       Type: System.Collections.Generic.Dictionary`2[System.String, System.Collections.Generic.List`1[System.String]]
+        // 1: Guyana: Banana, Plaintain, Mango, Cashew
+        // 2: New Zeeland: Kiwi, Apple, Avocado, Cherry
+        // 3: Ecuador: Banana
+        // 4: Spain: Persimmon, Orange, Mandarin
+        // 5: Canada: Blueberry
+        // 6: Russia: Strawberry
+        // 7: Israel: Persimmon, Dates, Pomegranates
+        // 8: China: Dragon Fruit, Apple, Pear, Pomelo, Cherry, Mandarin
+        // 9: Chile: Figs, Avocado, Blueberry, Cherry, Grape, Plum
+        //10: Guatemala: Pineapple, Banana, Papaya
+        //11: Mexico: Lemon, Tomato, Blueberry, Avocado
         static void dictionary()
+
         {
-         
-            int counter = 0;    
+
+            int counter = 0;
             Dictionary<String, List<string>> country = new Dictionary<string, List<string>>();
-            country.Add("Guyana", new List<string>{"Banana", "Plaintain", "Mango", "Cashew"} );
-            country.Add("New Zeeland", new List<string>{ " Kiwi", "Apple", " Avocado", " Cherry" } );
-            country.Add(" Ecuador", new List<string>{"Banana"} );
-            country.Add("Spain", new List<string>{ " Persimmon", " Orange", "Mango", " Mandarin" } );
-            country.Add("Canada", new List<string>{ " Blueberry"} );
-            country.Add("Russia", new List<string>{ " Strawberry"} );
-            country.Add("Israel", new List<string>{ " Persimmon", " Dates", " Pomegranates"} );
-            country.Add("China", new List<string>{ " Dragon Fruit", "Apple", "Pear", "Cherry","Mandarin"} );
-            country.Add("Chile", new List<string>{"Figs", " Avocado", " Blueberry", "Cherry","Ggrape","Plum" } );
-            country.Add("Guatemala", new List<string> { "Banana", "Pinapple", "Papaya" } );
-            country.Add("Mexico", new List<string> { "Lemon", " Tomato", "Blueberry","Avvado" } );
+            country.Add("Guyana", new List<string> { "Banana", "Plaintain", "Mango", "Cashew" });
+            country.Add("New Zeeland", new List<string> { " Kiwi", "Apple", " Avocado", " Cherry" });
+            country.Add(" Ecuador", new List<string> { "Banana" });
+            country.Add("Spain", new List<string> { " Persimmon", " Orange", "Mango", " Mandarin" });
+            country.Add("Canada", new List<string> { " Blueberry" });
+            country.Add("Russia", new List<string> { " Strawberry" });
+            country.Add("Israel", new List<string> { " Persimmon", " Dates", " Pomegranates" });
+            country.Add("China", new List<string> { " Dragon Fruit", "Apple", "Pear", "Cherry", "Mandarin" });
+            country.Add("Chile", new List<string> { "Figs", " Avocado", " Blueberry", "Cherry", "Ggrape", "Plum" });
+            country.Add("Guatemala", new List<string> { "Banana", "Pinapple", "Papaya" });
+            country.Add("Mexico", new List<string> { "Lemon", " Tomato", "Blueberry", "Avvado" });
 
 
 
             foreach (var item in country)
             {
                 counter++;
-                Console.WriteLine($"{counter} : {item.Key}: {string.Join(",",item.Value)}");
+                Console.WriteLine($"{counter} : {item.Key}: {string.Join(",", item.Value)}");
             }
 
         }
+
+        // 1: Banana: Guyana, Ecuador, Guatemala
+        //   2: Plaintain: Guyana
+        //   3: Mango: Guyana
+        //   4: Cashew: Guyana
+        //   5: Kiwi: New Zeeland
+        //   6: Apple: New Zeeland, China
+        //   7: Avocado: New Zeeland, Chile, Mexico
+        //   8: Cherry: New Zeeland, China, Chile
+        //   9: Persimmon: Spain, Israel
+        //  10: Orange: Spain
+        //  11: Mandarin: Spain, China
+        //  12: Blueberry: Canada, Chile, Mexico
+        //  13: Strawberry: Russia
+        //  14: Dates: Israel
+        //  15: Pomegranates: Israel
+        //  16: Dragon Fruit: China
+        //  17: Pear: China
+        //  18: Pomelo: China
+        //  19: Figs: Chile
+        //  20: Grape: Chile
+        //  21: Plum: Chile
+        //  22: Pineapple: Guatemala
+        //  23: Papaya: Guatemala
+        //  24: Lemon: Mexico
+        //  25: Tomato: Mexico
+
+        static void orinigalCountry()
+
+        {
+
+            int counter = 0;
+            Dictionary<String, List<string>> country = new Dictionary<string, List<string>>();
+            country.Add("Banana", new List<string> { "Guyana", "Ecuador", "Guatemala" });
+            country.Add("Mango", new List<string> { "Guyana" });
+            country.Add("Cashew", new List<string> { "Guyana" });
+            country.Add("Kiwi", new List<string> { "New Zeeland" });
+            country.Add("Apple", new List<string> { "New Zeeland", "China" });
+            country.Add("Avocado", new List<string> { " New Zeeland", "Chile", "Mexico" });
+            country.Add("Cherry", new List<string> { " New Zeeland", "China", "Chile" });
+            country.Add("Persimmon", new List<string> { " Spain", "Israel" });
+            country.Add("Orange", new List<string> { "Spain" });
+            country.Add("Mandarin", new List<string> { " Spain", "China" });
+            country.Add("Blueberry", new List<string> { "Canada", "Chile", "Mexico" });
+            country.Add("Strawberry", new List<string> { " Russia" });
+            country.Add("Dates", new List<string> { " Israel" });
+            country.Add("Pomegranates", new List<string> { " Israel" });
+            country.Add("Dragon Fruit", new List<string> { "China" });
+            country.Add("Pear", new List<string> { "China" });
+            country.Add("Pomelo", new List<string> { "China" });
+            country.Add("Figs", new List<string> { "Chile" });
+            country.Add("Grape", new List<string> { "Chile" });
+            country.Add("Plum", new List<string> { "Chile" });
+            country.Add("Pineapple", new List<string> { "Guatemala" });
+            country.Add("Papaya", new List<string> { "Guatemala" });
+            country.Add("Lemon", new List<string> { " Mexico" });
+            country.Add("Tomato", new List<string> { " Mexico" });
+
+            foreach (var item in country)
+            {
+                counter++;
+                Console.WriteLine($"{counter} : {item.Key}: {string.Join(",", item.Value)}");
+            }
+
+        }
+
 
     }
 }
