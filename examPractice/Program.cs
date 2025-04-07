a82bcbad088c7f9ffad5746f890df8c2e8b57907
lightknightdark <lightknightdark@gmail.com>
2025-04-07 14:18:45 -0400

    q1

--- examPractice/Program.cs
+++ examPractice/Program.cs
@@ -10,6 +10,8 @@ namespace examPractice
     {
         static void Main(string[] args)
         {
+         //   hashSet();
+            dictionary();
         }
 
 
@@ -43,6 +45,7 @@ namespace examPractice
         // 25: Tomato
 
         static void hashSet() {
+            int counter = 0;
             HashSet<String> fruit = new HashSet<String>();
             fruit.Add("Banana");
             fruit.Add("Plaintain");
@@ -55,17 +58,68 @@ namespace examPractice
             fruit.Add("Persimmon");
             fruit.Add("Orange");
             fruit.Add("Mandarin");
-            fruit.Add("Plaintain");
-            fruit.Add("Plaintain");
-            fruit.Add("Plaintain");
-            fruit.Add("Plaintain");
-            fruit.Add("Plaintain");
-            fruit.Add("Plaintain");
-            fruit.Add("Plaintain");
-            fruit.Add("Plaintain");
-            fruit.Add("Plaintain");
-            fruit.Add("Plaintain");
+            fruit.Add("Blueberry");
+            fruit.Add("Strawberry");
+            fruit.Add("Dates");
+            fruit.Add("Pomegranates");
+            fruit.Add("Dragon Fruit");
+            fruit.Add("Pear");
+            fruit.Add("Pomelo");
+            fruit.Add("Figs");
+            fruit.Add("Grape");
+            fruit.Add("Plum");
+            fruit.Add("Pineapple");
+            fruit.Add("Papaya");
+            fruit.Add("Lemon");
+            fruit.Add("Tomato");
+
+            foreach (string item in fruit)
+            {
+                counter++;
+                Console.WriteLine($"{counter} : {item}");
+            }
+          
+        }
+
+        //Question 1b: Create a Dictionary of Countries with the fruits exports (as a list)
+ //       Type: System.Collections.Generic.Dictionary`2[System.String, System.Collections.Generic.List`1[System.String]]
+ // 1: Guyana: Banana, Plaintain, Mango, Cashew
+ // 2: New Zeeland: Kiwi, Apple, Avocado, Cherry
+ // 3: Ecuador: Banana
+ // 4: Spain: Persimmon, Orange, Mandarin
+ // 5: Canada: Blueberry
+ // 6: Russia: Strawberry
+ // 7: Israel: Persimmon, Dates, Pomegranates
+ // 8: China: Dragon Fruit, Apple, Pear, Pomelo, Cherry, Mandarin
+ // 9: Chile: Figs, Avocado, Blueberry, Cherry, Grape, Plum
+ //10: Guatemala: Pineapple, Banana, Papaya
+ //11: Mexico: Lemon, Tomato, Blueberry, Avocado
+        static void dictionary()
+        {
+         
+            int counter = 0;    
+            Dictionary<String, List<string>> country = new Dictionary<string, List<string>>();
+            country.Add("Guyana", new List<string>{"Banana", "Plaintain", "Mango", "Cashew"} );
+            country.Add("New Zeeland", new List<string>{ " Kiwi", "Apple", " Avocado", " Cherry" } );
+            country.Add(" Ecuador", new List<string>{"Banana"} );
+            country.Add("Spain", new List<string>{ " Persimmon", " Orange", "Mango", " Mandarin" } );
+            country.Add("Canada", new List<string>{ " Blueberry"} );
+            country.Add("Russia", new List<string>{ " Strawberry"} );
+            country.Add("Israel", new List<string>{ " Persimmon", " Dates", " Pomegranates"} );
+            country.Add("China", new List<string>{ " Dragon Fruit", "Apple", "Pear", "Cherry","Mandarin"} );
+            country.Add("Chile", new List<string>{"Figs", " Avocado", " Blueberry", "Cherry","Ggrape","Plum" } );
+            country.Add("Guatemala", new List<string> { "Banana", "Pinapple", "Papaya" } );
+            country.Add("Mexico", new List<string> { "Lemon", " Tomato", "Blueberry","Avvado" } );
+
+
+
+            foreach (var item in country)
+            {
+                counter++;
+                Console.WriteLine($"{counter} : {item.Key}: {string.Join(",",item.Value)}");
+            }
 
         }
+
     }
 }
